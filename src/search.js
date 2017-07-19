d03cbbc6391ef9d2f9e01bc674c7b9d85e912eca
Max Gerber <mgerber94@gmail.com>
2017-07-19 16:26:40 +0100

    created password array in array.js created search filter function wrote TDD testing search function relates #10'

--- src/search.js
+++ src/search.js
@@ -1,3 +1,10 @@
-//read the file
-//filter.function
-//limitting the number of responses;
+const passwords = require('./passwords.js');
+
+const passwordFilter = function (array, input) {
+  const newPasswords = array.filter(
+    function(passwordString) {
+      return passwordString.slice(0, input.length) === input;
+    });
+  return newPasswords;
+};
+module.exports = passwordFilter;
--- src/test.js
+++ src/test.js
@@ -1,3 +1,15 @@
-//test the search;
-//reference link:
-//https://github.com/foundersandcoders/modules-challenge/blob/solutions/test/test.js
+// test the search;
+// reference link:
+// https://github.com/foundersandcoders/modules-challenge/blob/solutions/test/test.js
+
+const test = require('tape');
+const search = require('./search');
+// const passwords = require('./passwords');
+
+test('newPasswords test', function (t) {
+  const array = ['qwerty', 'yes', 'no', 'maybe', 'i dont know'];
+  const actual = search(array, 'q');
+  const expected = ['qwerty'];
+  t.deepEqual(actual, expected, 'newPasswords test should return array of strings which match filter');
+  t.end();
+});
