185f5c25ab6bde27028c166ee25568b42819c392
Max Gerber <mgerber94@gmail.com>
2017-07-19 16:30:04 +0100

    converted filter to ES6 format relates #10

--- src/search.js
+++ src/search.js
@@ -1,10 +1,8 @@
 const passwords = require('./passwords.js');
 
-const passwordFilter = function (array, input) {
-  const newPasswords = array.filter(
-    function(passwordString) {
-      return passwordString.slice(0, input.length) === input;
-    });
+const passwordFilter = (array, input) => {
+  const newPasswords = array.filter(passwordString =>
+    passwordString.slice(0, input.length) === input);
   return newPasswords;
 };
 module.exports = passwordFilter;
