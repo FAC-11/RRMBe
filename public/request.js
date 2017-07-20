e29376ea39dc4c3998f1b7146cbc711fa1cf3063
rachaelcodes <rorourke.rsp@gmail.com>
2017-07-20 10:54:10 +0100

    create request.js relates #5

--- public/request.js
+++ public/request.js
@@ -1,3 +1,17 @@
-//get request 'xhr request';
-// callback --> calls the dom to send the response;
-//see gitter;
+function serverRequest(url, cb) {
+  var xhr = new XMLHttpRequest();
+
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        cb(null, xhr.response);
+      } else {
+        console.log('thats an error...', xhr);
+        cb(xhr.status, xhr.response);
+      }
+    }
+
+    xhr.open('GET', url, true);
+    xhr.send();
+  };
+}
