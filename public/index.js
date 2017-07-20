c7e5c8b6ae4448b76e24d0904945469102ab81e7
rachaelcodes <rorourke.rsp@gmail.com>
2017-07-20 12:52:41 +0100

    added callback function relates #3

--- public/index.js
+++ public/index.js
@@ -1,43 +1,26 @@
 (function () {
   var searchInput = document.getElementsByName('search-input')[0];
   var dataList = document.getElementById('password-examples');
+  var messageBoard = document.getElementById('messages');
   var passwordString = '';
-  var renderDOM = function (err, res) {
-    if (err) {
-      
-    }
-  }
 
-  searchInput.addEventListener('keyup', function(e) {
+  var renderDOM = function (error, result) {
+    if (error) {
+      messageBoard.appendChild(result);
+    } else {
+      var responseArray = result.split(',');
+      responseArray.forEach(function (password) {
+        var passwordExample = document.createElement('option');
+        var passwordText = document.createTextNode(password);
+        passwordExample.appendChild(passwordText);
+        dataList.appendChild(passwordExample);
+      });
+    }
+  };
+
+  searchInput.addEventListener('keyup', function (e) {
     passwordString = '/search/' + encodeURIComponent(e.target.value);
 
-  serverRequest(passwordString, renderDOM);
-  //   if (e.key.length === 1 || e.key === "Backspace" || e.key === "Space") {
-  //   switch(e.key) {
-  //     case 'Backspace':
-  //       if (passwordString.length > 0 ) {
-  //         passwordString = passwordString.slice(0, -1);
-  //       }
-  //       break;
-  //     case '"':
-  //       passwordString += '\"';
-  //       break;
-  //     case "'":
-  //       passwordString += "\'";
-  //       break;
-  //     case '\\':
-  //       passwordString += '\\';
-  //       break;
-  //     case 'Space':
-  //       passwordString += ' ';
-  //       break;
-  //     // case ( 'Shift' || 'Meta' || 'Alt' || 'ArrowLeft' || 'ArrowRight' || 'ArrowUp' || ''):
-  //     //   break;
-  //     default:
-  //       passwordString += e.key;
-  //   }
-  // }
-  //   console.log(passwordString);
-  //
+    serverRequest(passwordString, renderDOM);
   });
 })();
