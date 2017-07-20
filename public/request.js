cbc87d45b30f3ad5e8dec351c34df3bcd92091a8
rachaelcodes <rorourke.rsp@gmail.com>
2017-07-20 14:37:28 +0100

    removed unnecessary console.logs)

--- public/request.js
+++ public/request.js
@@ -1,17 +1,14 @@
 function serverRequest(url, cb) {
-  console.log('url put into serverRequest', url);
   var xhr = new XMLHttpRequest();
   xhr.onreadystatechange = function () {
     if (xhr.readyState == 4) {
       if (xhr.status == 200) {
         cb(null, xhr.response);
       } else {
-        console.log('thats an error...', xhr);
         cb(xhr.status, xhr.response);
       }
     }
   };
-  console.log('xhr', xhr);
   xhr.open('GET', url, true);
   xhr.send();
 }
