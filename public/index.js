f7153231476a328a1c8f5fbf482552750d64cbdd
Max Gerber <mgerber94@gmail.com>
2017-07-20 12:11:05 +0100

    Started writing dom js, encoded search query into url-friendly format Relates #2

--- public/index.js
+++ public/index.js
@@ -1,4 +1,43 @@
-// iffe
-// event listener that sends the input -url onkeyup
-// dom.render function --> called by request. js (takes the data and puts it in the dom);
-// event listener for on.submit
+(function () {
+  var searchInput = document.getElementsByName('search-input')[0];
+  var dataList = document.getElementById('password-examples');
+  var passwordString = '';
+  var renderDOM = function (err, res) {
+    if (err) {
+
+    }
+  }
+
+  searchInput.addEventListener('keyup', function(e) {
+    passwordString = '/search/' + encodeURIComponent(e.target.value);
+
+  serverRequest(passwordString, renderDOM);
+  //   if (e.key.length === 1 || e.key === "Backspace" || e.key === "Space") {
+  //   switch(e.key) {
+  //     case 'Backspace':
+  //       if (passwordString.length > 0 ) {
+  //         passwordString = passwordString.slice(0, -1);
+  //       }
+  //       break;
+  //     case '"':
+  //       passwordString += '\"';
+  //       break;
+  //     case "'":
+  //       passwordString += "\'";
+  //       break;
+  //     case '\\':
+  //       passwordString += '\\';
+  //       break;
+  //     case 'Space':
+  //       passwordString += ' ';
+  //       break;
+  //     // case ( 'Shift' || 'Meta' || 'Alt' || 'ArrowLeft' || 'ArrowRight' || 'ArrowUp' || ''):
+  //     //   break;
+  //     default:
+  //       passwordString += e.key;
+  //   }
+  // }
+  //   console.log(passwordString);
+  //
+  });
+})();
