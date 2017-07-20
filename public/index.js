6495954edb04c55251543d6616d89ae6152b06dc
Rebecacalvoquintero <rebecacalvoquintero@hotmail.es>
2017-07-20 15:49:25 +0100

    message1 created

--- public/index.js
+++ public/index.js
@@ -2,9 +2,9 @@
   var searchInput = document.getElementsByName('search-input')[0];
   var dataList = document.getElementById('password-examples');
   var messageBoard = document.getElementById('messages');
+  var form = document.getElementById('passwordForm');
 
   var renderDOM = function (error, result) {
-    console.log('renderDOM is running')
     if (error) {
       messageBoard.appendChild(result);
     } else {
@@ -23,4 +23,23 @@
     passwordString = '/search/' + encodeURIComponent(e.target.value);
     serverRequest(passwordString, renderDOM);
   });
+
+  form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    console.log(document.getElementById('password-examples'));
+    var heading = document.createElement('h2');
+    var headingText = document.createTextNode('WARNING!');
+    var passwordTry = e.target["0"].form["0"].value;
+    // if(passwordTry === document.getElementById('password-examples')){
+    //   console.log(yay!);
+    // }
+    var text = document.createTextNode('We are sorry but ' + passwordTry + ' is one of the top 1000 used passwords. Try something else!')
+    while(messageBoard.hasChildNodes()){
+      messageBoard.removeChild(messageBoard.lastChild);
+    }
+    heading.appendChild(headingText);
+    messageBoard.appendChild(heading);
+    messageBoard.appendChild(text);
+  })
+
 })();
